c99cdf2474dce9cadede4a34fa865d8973c472c3
Daniel Carty <dan120.c1e@gmail.com>
2024-01-19 18:37:34 +0000

    Add files via upload

---
+++ details.py
@@ -0,0 +1,9 @@
+# Created variables to request input of user's name, age, house number and  street name. 
+ 
+name = input("Enter Full Name: ")
+age = input("Enter Age: ")
+house_num = input("Enter House Number: ")
+street = input("Enter Street: ") 
+# Confirmed the user's data in a statement based on the variables.
+print()
+print("This is " + name + ".  He/she is " + age + " years old and lives at house number " + house_num + " on " + street + ".")
